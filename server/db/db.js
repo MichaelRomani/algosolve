e6b4cdf8505f6ece0ac7b8ec45a50e5eb04b6726
Mike Romani <mr4227@gmail.com>
2018-05-22 12:31:27 -0400

    changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/algorithms', {
-    logging: false
-  }
-)
+const db = new Sequelize({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  database: process.env.DB_DATABASE,
+  dialectOptions: {
+    ssl: 'Amazon RDS'
+  },
+  pool: { maxConnections: 5, maxIdleTime: 30 },
+  language: 'en'
+});
+
 module.exports = db
